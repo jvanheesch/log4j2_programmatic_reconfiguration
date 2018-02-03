98a9c689b4470439b31c95d37a798d2331c9ecb4
Joris van Heesch <joris.vanheesch@hpcds.com>
2018-02-03 21:50:31 +0100

    basic bootstrap

--- webapp/src/main/java/com/github/jvanheesch/WicketApplication.java
+++ webapp/src/main/java/com/github/jvanheesch/WicketApplication.java
@@ -1,5 +1,7 @@
 package com.github.jvanheesch;
 
+import de.agilecoders.wicket.core.Bootstrap;
+import de.agilecoders.wicket.core.settings.BootstrapSettings;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
@@ -14,6 +16,10 @@ public class WicketApplication extends WebApplication {
     protected void init() {
         super.init();
 
+        this.getMarkupSettings().setStripWicketTags(true);
+
         this.getComponentInstantiationListeners().add(new SpringComponentInjector(this));
+
+        Bootstrap.install(this);
     }
 }
